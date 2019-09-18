5f5e48fd4bc7378f189d466efe26666726300dc2
SuperCapra <gvnnmaggi@gmail.com>
2019-09-18 10:43:08 +0200

    Exercize for ToUpper e ToLower

---
+++ src/s045/Es.java
@@ -0,0 +1,55 @@
+package s045;
+
+public class Es {
+    final static int diff = 'a' - 'A';
+
+
+    public static void main(String[] args) {
+        exercise("Bob");
+        exercise("ciAo");
+        
+        Es es = new Es(); //per creare l'oggetto Es
+
+        System.out.println("My toUpper(): " + es.toUpper("Bob")); //per essere pignolo bisogna scrivere Es.toUpper("Bob")
+    }
+
+    public String toUpper(String s) { //se tolgo 'static' è diventato di istanza
+        StringBuilder sb = new StringBuilder(s.length()); //facciamo così per non creare altre stringhe e quindi non allochiamo spazio per nulla
+
+        for (int i = 0; i < s.length(); i++) {
+            char cur = s.charAt(i);
+            if (cur >= 'a' && cur <= 'z') {
+                sb.append((char) (cur - diff)); //qua serve specificare il (char) per convertire il numero in carattere
+            } else {
+                sb.append(cur);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static void exercise(String sample) {
+        System.out.println(sample + " to uppercase and lowercase");
+        for (int i = 0; i < sample.length(); i++) {
+            char cur = sample.charAt(i);
+            if (cur >= 'a' && cur <= 'z') {
+                System.out.print((char) (cur - diff));
+            } else {
+                System.out.print(cur);
+            }
+        }
+
+        System.out.print(", ");
+
+        for (int i = 0; i < sample.length(); i++) {
+            char cur = sample.charAt(i);
+            if (cur >= 'A' && cur <= 'Z') {
+                System.out.print((char) (cur + diff));
+            } else {
+                System.out.print(cur);
+            }
+        }
+
+        System.out.println();
+    }
+}
---
+++ src/s045/EsVer2.java
@@ -0,0 +1,101 @@
+package s045;
+
+public class EsVer2 {
+    final static int GAP = 'a' - 'A';
+
+    public static void main(String[] args) {
+        exercise("Bob");
+        exercise("ciAo");
+
+        System.out.println("My toUpper(): " + toUpper("Bob"));
+        System.out.println("My toUpper2(): " + toUpper2("Bob"));
+        System.out.println("My toUpper3(): " + toUpper3("Bob"));
+        System.out.println("My toUpper4(): " + toUpper4("Bob"));
+    }
+    
+    public static String toUpper(String s) {
+        StringBuilder sb = new StringBuilder(s.length());
+
+        for (int i = 0; i < s.length(); i++) {
+            char cur = s.charAt(i);
+            if (cur >= 'a' && cur <= 'z') {
+                sb.append((char) (cur - GAP));
+            } else {
+                sb.append(cur);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static String toUpper2(String s) {
+        StringBuilder sb2 = new StringBuilder(s);
+
+        for (int i = 0; i < s.length(); i++) {
+        	char cur = s.charAt(i);
+            if ( cur >= 'a' &&  cur <= 'z') {
+            	sb2.setCharAt(i, (char) (cur-GAP));
+            }
+            else {
+            	sb2.setCharAt(i, cur);
+            }
+        }
+
+        return sb2.toString();
+    }
+    
+    public static String toUpper3(String s) {
+        StringBuilder sb3 = new StringBuilder(s.length());
+
+        for (int i = 0; i < s.length(); i++) {
+        	char cur = s.charAt(i);
+            if ( cur >= 'a' &&  cur <= 'z') {
+            	sb3.insert(i, cur);
+            }
+            else { 
+            	sb3.insert(i, cur);
+            }
+        }
+
+        return sb3.toString();
+    }
+    
+    public static String toUpper4(String s) {
+        StringBuilder sb4 = new StringBuilder(s);
+
+        for (int i = 0; i < s.length(); i++) {
+        	char cur = s.charAt(i);
+            if ( cur >= 'a' &&  cur <= 'z') {
+            	String x = Character.toString((char) (cur - GAP));
+            	sb4.replace(i, i+1, x);
+            }
+        }
+
+        return sb4.toString();
+    }
+
+    public static void exercise(String sample) {
+        System.out.println(sample + " to uppercase and lowercase");
+        for (int i = 0; i < sample.length(); i++) {
+            char cur = sample.charAt(i);
+            if (cur >= 'a' && cur <= 'z') {
+                System.out.print((char) (cur - GAP));
+            } else {
+                System.out.print(cur);
+            }
+        }
+
+        System.out.print(", ");
+
+        for (int i = 0; i < sample.length(); i++) {
+            char cur = sample.charAt(i);
+            if (cur >= 'A' && cur <= 'Z') {
+                System.out.print((char) (cur + GAP));
+            } else {
+                System.out.print(cur);
+            }
+        }
+
+        System.out.println();
+    }
+}
