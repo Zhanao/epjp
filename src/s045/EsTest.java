0b93d0b60f26b1b716d57add8068e4c9ab2a57b6
SuperCapra <gvnnmaggi@gmail.com>
2019-09-18 12:01:29 +0200

    test non corretto pichè non avevo definito la classe statica

---
+++ src/s045/EsTest.java
@@ -0,0 +1,14 @@
+package s045;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EsTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
