8f50c30bdbef8b31e8c319786775a714eb634763
SuperCapra <gvnnmaggi@gmail.com>
2019-09-19 17:52:32 +0200

    nuovo

--- src/aba/Aaa.java
+++ src/aba/Aaa.java
@@ -1,5 +1,21 @@
-package aba;
+package aba; //prima cosa si mette il package
 
-public class Aaa {
+public class Aaa { //seconda cosa si crea la classe
+	public static void main(String[] args) { //il main serve per far vivere la classe autonomamente
+		
 
+	System.out.println("");
+	
+	Aaa.f();
+	}
+	public static int f() {
+		return 3;
+	}
 }
+
+//class Aba {
+//	public static void main(String[] args) { //si può mettere il main anche qua
+//		
+//	}
+//	
+//}
