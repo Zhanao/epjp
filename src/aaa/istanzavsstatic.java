0bd33b4288edde2408b1e03cba1afa9c246e134e
Zetha <Zetha@Zetha-PC.station>
2019-09-19 19:41:22 +0200

    19.9.2019e

---
+++ src/aaa/istanzavsstatic.java
@@ -0,0 +1,24 @@
+package aaa;
+
+public class istanzavsstatic {
+	// scivo main, e schiaccio ctrl+space
+	public static void main(String[] args) {
+		System.out.print("ciao");
+		istanzavsstatic test= new istanzavsstatic();
+		
+		System.out.println(test.f()); // scrivere " sysout " e schiacciare ctrl+space
+					
+		
+					// dato che f() e' un metodo di istanza e non statico devo per forza definire un oggetto per farlo funzionare
+				  // oppure in alternativa rendo f() static: public static int (); e per chiamarlo basta scrivere A.f():
+		
+		
+//System.out.println( A.f());
+	
+	}
+	
+	public /* static */ int f() {
+		return 11;
+	}
+
+}
