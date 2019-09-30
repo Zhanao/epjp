5ef8a31b27901da97c0e94a7beafd9b321d9d07b
Administrator <Administrator@192.168.20.56>
2019-09-30 17:57:58 +0200

    30.09.2019b

--- sqlpersonale/hr.sql
+++ sqlpersonale/hr.sql
@@ -211,3 +211,9 @@ select first_name, last_name, department_name
 from employees right outer join departments -- considera anche treasury che non ha dipendenti in quel dipartimento
 using(department_id)
 where department_id between 110 and 120;
+
+select e.last_name, d.department_name
+from employees e full outer join departments d
+on (e.department_id = d.department_id)
+where last_name = 'Grant'
+or d.department_id between 110 and 120;
