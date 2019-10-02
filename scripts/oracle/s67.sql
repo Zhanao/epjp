ae5e92241da01d056d6f583ff221af292b653d2f
Administrator <Administrator@192.168.20.60>
2019-10-02 12:29:11 +0200

    some script update morning 2 of october

--- scripts/oracle/s67.sql
+++ scripts/oracle/s67.sql
@@ -5,25 +5,47 @@ create table users (
     gender char(1)
 );
 
-insert into users values(92, 4, 'M');
-insert into users values(32, 3, 'F');
-insert into users values(31, 2, 'F');
-insert into users values(14, 3, 'M');
+insert into users 
+values(92, 4, 'M');
+
+insert into users 
+values(32, 3, 'F');
+
+insert into users 
+values(31, 2, 'F');
+
+insert into users 
+values(14, 3, 'M');
+
+select *
+from users;
 
 -- btree index
+--gli alber in java erano nella ram
+--in SQL invece le tabelle saranno sul disco fisso
+--i btree invece servono per mettere un po' i valori sulla ram e un po' sull'hd
+--gli indici sono una delle cose più variabili delle tabelle, infatti ci sono delle persone per sistemare gli indici, 
+--attenzione gli indici rallentano molto le cose quindi bisogna stare attenti se o come inserirli
 
-create index users_id_ix on users(id);
+--indice semplice
+create index users_id_ix 
+on users(id);
 
-create index users_id_code_ix on users(id, code);
+--indice composto
+create index users_id_code_ix 
+on users(id, code);
 
 drop index users_id_ix;
 
 
 -- bitmap index
 
-create bitmap index users_gender_ix on users(gender);
+create bitmap index users_gender_ix 
+on users(gender);
 
-select * from users order by gender;
+select * 
+from users 
+order by gender;
 
 -- cleanup
 drop table users;
