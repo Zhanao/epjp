e6233538ee182de237a31fed368152a108d94786
Administrator <Administrator@192.168.20.60>
2019-10-02 16:50:08 +0200

    afternoon 2 october

--- scripts/oracle/s73.sql
+++ scripts/oracle/s73.sql
@@ -1,14 +1,29 @@
 set serveroutput on
+set serveroutput off
 
 begin
     dbms_output.put_line('Hello PL/SQL');
 end;
 /
 
+--solitamente si usa il v_ per iniziare le varaibili, dove v sta per variable
+--solitamente le variabili si inizializzano del declare
+--in PL/SQL si possono usare anche i BOOLEAN
+--la divisione tra interi fa un round
 declare
     v_width integer;
-    v_height integer := 2;
-    v_area integer := 6;
+    v_height integer := 3;
+    v_area integer := 10;
+begin
+    v_width := v_area / v_height;
+    dbms_output.put_line('v_width = ' || v_width);
+end;
+/
+
+declare
+    v_width float;
+    v_height integer := 3;
+    v_area integer := 10;
 begin
     v_width := v_area / v_height;
     dbms_output.put_line('v_width = ' || v_width);
--- scripts/oracle/s76.sql
+++ scripts/oracle/s76.sql
@@ -12,3 +12,47 @@ begin
     end if;
 end;
 /
+
+declare
+    v_a integer = 0;
+begin
+    if v_a > 0 then
+        dbms_output.put_line('v_a is positive');
+    elsif v_a = 0 then
+        dbms_output.put_line('v_a is zero');
+    else
+        dbms_output.put_line('v_a is negative');
+    end if;
+end;
+/
+
+declare
+    v_a integer := -5;
+begin
+    if v_a > 0 then
+        dbms_output.put_line('v_a is positive');
+    elsif v_a = 0 then
+        dbms_output.put_line('v_a is zero');
+    else
+        dbms_output.put_line('v_a is negative');
+    end if;
+end;
+/
+
+--per tirare l'eccezzione
+declare
+    v_a integer;
+begin
+    v_a := 9999999999999999999999999999999999999999999999999;
+    if v_a > 0 then
+        dbms_output.put_line('v_a is positive');
+    elsif v_a = 0 then
+        dbms_output.put_line('v_a is zero');
+    else
+        dbms_output.put_line('v_a is negative');
+    end if;
+exception
+    when others then
+    dbms_output.put_line('Exception');
+end;
+/
